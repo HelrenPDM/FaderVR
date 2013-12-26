67a0f42b1e746107a2c4ca31f83c91938b17af76
Steven Yau <gambit@stevenyau.co.uk>
2013-12-26 18:04:46 +0000

    First commit of project

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+using System.Collections.Generic;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		TwitterAPI.instance.SearchTwitter("#GDC", ResultsCallBack);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void ResultsCallBack(List<TwitterData> tweetList) {
+		Debug.Log("====================================================");
+		foreach(TwitterData twitterData in tweetList) {
+			Debug.Log("Tweet: " + twitterData.ToString());
+		}
+	}
+}
