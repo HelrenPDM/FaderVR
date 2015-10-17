1579b30435efbc9dce597871a9f6b004a5a2c7fb
Stephan Gensch <stgensch@vragments.com>
2015-10-17 18:47:32 +0200

    media item scene

---
+++ Assets/Scripts/SQLite/SQLiteWrapper.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Mono.Data.SqliteClient;
+
+public class SQLiteWrapper : MonoBehaviour
+{
+
+	private string _constr = "URI=file:NPCMaster.db";
+	//private IDbConnection _dbc;
+	//private IDbCommand _dbcm;
+	//private IDataReader _dbr;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
