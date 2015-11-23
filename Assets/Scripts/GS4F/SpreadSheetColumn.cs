81a31b846fd8df126cbbf1674dd2e9291898fed8
Stephan Gensch <stgensch@vragments.com>
2015-11-23 19:05:21 +0100

    some spreadsheet adaptions

---
+++ Assets/Scripts/GS4F/SpreadSheetColumn.cs
@@ -0,0 +1,162 @@
+//
+// SpreadSheetColumn.cs
+//
+// Author:
+//       Stephan Gensch <stgensch@vragments.com>
+//
+// Copyright (c) 2015 Stephan Gensch
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Fader
+{
+    [Serializable]
+    public abstract class SpreadSheetColumn<PayloadType>
+	{
+        public string ColumnHeader;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private PayloadType m_TypeData;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public event EventHandler<FaderEventArg<PayloadType>> DataChangedHandler;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        abstract public PayloadType GetData ();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        abstract protected void setDataModel (PayloadType value);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        public void SetCurrentData (PayloadType value)
+        {
+            setDataModel (value);
+            fireDataChangedEvent (GetData ());
+            m_TypeData = GetData ();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        virtual protected void Awake ()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        virtual protected void Start ()
+        {
+            m_TypeData = GetData ();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        void Update ()
+        {
+            PayloadType currentData = GetData ();
+            if (!compare (m_TypeData, currentData))
+            {
+                fireDataChangedEvent (currentData);
+            }
+            m_TypeData = currentData;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="currentData"></param>
+        protected void fireDataChangedEvent (PayloadType currentData)
+        {
+            EventHandler<FaderEventArg<PayloadType>> handler = DataChangedHandler;
+            if (handler != null)
+            {
+                handler (this, new FaderEventArg<PayloadType> (currentData));
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        private bool compare (PayloadType x, PayloadType y)
+        {
+            return EqualityComparer<PayloadType>.Default.Equals (x, y);
+        }
+    }
+
+    public class SpreadSheetColumnDateTime : SpreadSheetColumn<DateTime>
+    {
+        public override DateTime GetData ()
+        {
+            return GetData ();
+        }
+
+        protected override void setDataModel (DateTime value)
+        {
+            throw new NotImplementedException ();
+        }
+    };
+
+    public class SpreadSheetColumnString : SpreadSheetColumn<string>
+    {
+        public override string GetData ()
+        {
+            throw new NotImplementedException ();
+        }
+
+        protected override void setDataModel (string value)
+        {
+            throw new NotImplementedException ();
+        }
+    }
+
+    public class SpreadSheetColumnLong : SpreadSheetColumn<long>
+    {
+        public override long GetData ()
+        {
+            throw new NotImplementedException ();
+        }
+
+        protected override void setDataModel (long value)
+        {
+            throw new NotImplementedException ();
+        }
+    }
+}
---
+++ Assets/Scripts/GS4F/SpreadSheetReader.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+using MiniJSON;
+
+namespace Fader
+{
+    public class SpreadSheetReader : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start ()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update ()
+        {
+
+        }
+
+        // Use of MINI JSON http://forum.unity3d.com/threads/35484-MiniJSON-script-for-parsing-JSON-data
+        private List<SpreadSheetColumn> ParseResultsFromSpreadSheet (string jsonResults)
+        {
+            Debug.Log (jsonResults);
+            List<SpreadSheetColumn> sheetDataList = new List<SpreadSheetColumn> ();
+
+            foreach (var entry in sheetDataList)
+            {
+                Debug.Log (entry.ColumnHeader);
+            }
+
+            IDictionary search = (IDictionary)Json.Deserialize (jsonResults);
+            IList years = (IList)search ["yearbook_item"];
+            foreach (IDictionary year in years)
+            {
+                SpreadSheetColumn columnBase = new SpreadSheetColumn (year, SpreadSheet.CellType.);
+                columnBase.TweetID = (Int64)tweet ["id"];
+                columnBase.ScreenName = userInfo ["screen_name"] as string;
+                columnBase.CreationDate = DateTime.ParseExact (tweet ["created_at"] as string, Const_TwitterDateTemplate, new System.Globalization.CultureInfo ("en-US"));
+                columnBase.TweetText = tweet ["text"] as string;
+                columnBase.RetweetCount = (Int64)tweet ["retweet_count"];
+                columnBase.ProfileImageUrl = userInfo ["profile_image_url"] as string;
+
+                sheetDataList.Add (columnBase);
+            }
+
+            return sheetDataList;
+        }
+    }
+}
+
